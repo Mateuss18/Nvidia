9b931fcfa2407613fbbc224216ebf537819631f5
Mateuss18 <mateusdfl18@gmail.com>
2022-03-06 19:14:33 -0300

    Efeito slide automatico

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-let balls = document.querySelector('.balls')
-let quant = document.querySelector('.slides image')
-let atual = 0
-let images = document.getElementById('atual')
+let time = 4000,
+    currentSlideIndex = 0,
+    slide = document.querySelectorAll('.slides .slide')
+    ball = document.querySelectorAll('.balls .ball')
+    max = slide.length
 
+function nextSlide(){
+    slide[currentSlideIndex]
+        .classList.remove("selected")
+    ball[currentSlideIndex]
+        .classList.remove("selecionado")
+    currentSlideIndex++
+
+    if(currentSlideIndex >= max){
+        currentSlideIndex = 0
+    }
+
+    slide[currentSlideIndex]
+        .classList.add("selected")
+    ball[currentSlideIndex]
+        .classList.add("selecionado")
+}
+
+function start(){
+    setInterval(() => {
+        nextSlide()
+    }, time)
+}
+
+window.addEventListener("load", start)
